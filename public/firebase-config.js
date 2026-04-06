56d4993bd3a2fc83c074b16798580f74d562a0d0
istiaqueahmed231 <istiaque.ahmed.833845761@gmail.com>
2026-04-07 01:58:36 +0600

    chore: Include firebase client config

---
+++ public/firebase-config.js
@@ -0,0 +1,10 @@
+// TODO: Replace the placeholder values with your Firebase project configuration.
+// You can find these in the Firebase console under Project Settings -> General -> Your apps.
+export const firebaseConfig = {
+    apiKey: "AIzaSyDwnQj_7B2-cp7qz4wVLOW92AGMXBAuA9Q",
+  authDomain: "geo-aid-hub.firebaseapp.com",
+  projectId: "geo-aid-hub",
+  storageBucket: "geo-aid-hub.firebasestorage.app",
+  messagingSenderId: "402539835398",
+  appId: "1:402539835398:web:cb63870c08ab446b751387"
+};
